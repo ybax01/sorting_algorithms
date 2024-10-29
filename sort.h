5b1cbc5e652028928368a7eb4172e322fe37f8ae
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-10-29 18:38:09 +0100

    ss

--- sort.h
+++ sort.h
@@ -1,3 +1,6 @@
+#ifndef SORT_H
+#define SORT_H
+
 #include <stddef.h>
 
 /**
@@ -9,9 +12,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -32,3 +35,7 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+
+
+
+#endif /* SORT_H */
